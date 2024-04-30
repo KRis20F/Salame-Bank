747c13a51c0d66cff9b90710d047fe3153fd7dcd
KRis20f <cristiangutierrez008@gmail.com>
2024-04-30 09:55:07 +0200

    Agregando cositas

--- functions.py
+++ functions.py
@@ -1,37 +1,39 @@
 import mysql.connector
 
 def connect_database():
-    db = mysql.connector.connect(
+    conn = mysql.connector.connect(
         host = "localhost",
         user = "root",
         password = "",
         database = "salame_bank"
     )
-    return db
+    return conn
 
 def register_client(name, surname, age, country):
-    bd = connect_database()
-    cursor = bd.cursor()
+    database = connect_database()
+    cursor = database.cursor()
 
-    query = f"INSERT INTO clients\
-        VALUES(%s, %s, %s, %s);"
+    query = "INSERT INTO clients (name, surname, age, country) VALUES (%s, %s, %s, %s)"
     values = (name, surname, age, country)
     cursor.execute(query, values)
 
-    bd.commit()
-    bd.close()
+    print("cliente registrado")
+
+    database.commit()
+    database.close()
 
 def register_account(username, password, currency, vip):
-    bd = connect_database()
-    cursor = bd.cursor()
+    database = connect_database()
+    cursor = database.cursor()
 
-    query = f"INSERT INTO accounts\
-        VALUES(%s, %s, %s, %s);"
+    query = "INSERT INTO accounts (username, password, currency, vip) VALUES (%s, %s, %s, %s)"
     values = (username, password, currency, vip)
     cursor.execute(query, values)
 
-    bd.commit()
-    bd.close()
+    print("cuenta registrada")
+
+    database.commit()
+    database.close()
 
 def connect_account(user, password):
     pass
