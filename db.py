0d10e5b34d883a5b98a1c9bf1525fa7bc29becaa
alvarfs <alvaro.florez@estudiant.fjaverianas.com>
2024-05-08 12:05:41 +0200

    transactions and triggers added

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
-import mysql.connector, functions
+import mysql.connector
+import functions
 
 def connect_mysql():
     conn = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = ""
+        host="localhost",
+        user="root",
+        password=""
     )
     return conn
 
@@ -12,7 +13,7 @@ def create_database():
     database = connect_mysql()
     cursor = database.cursor()
 
-    # cursor.execute("DROP DATABASE IF EXISTS salame_bank;")
+    cursor.execute("DROP DATABASE IF EXISTS salame_bank;")
     cursor.execute("SHOW DATABASES LIKE 'salame_bank'")
     exists = cursor.fetchone()
     if not exists:
@@ -27,7 +28,7 @@ def create_tables():
     database = functions.connect_database()
     cursor = database.cursor()
 
-    query = """
+    clients_table = """
         CREATE TABLE clients (
             id_client INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(40),
@@ -35,11 +36,10 @@ def create_tables():
             age INT,
             country VARCHAR(60),
             email VARCHAR(80) UNIQUE
-        )
+        );
     """
-    cursor.execute(query)
 
-    query = """
+    accounts_table = """
         CREATE TABLE accounts (
             id_account INT AUTO_INCREMENT PRIMARY KEY,
             id_client INT,
@@ -47,9 +47,54 @@ def create_tables():
             password VARCHAR(60),
             currency INT,
             FOREIGN KEY (id_client) REFERENCES clients(id_client)
-        )
+        );
     """
-    cursor.execute(query)
+
+    transactions_table = """
+        CREATE TABLE transactions (
+            id_transactions INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(20),
+            old_currency INT,
+            new_currency INT,
+            date DATE,
+            time TIME,
+            balance INT
+        );
+    """
+
+    cursor.execute(clients_table)
+    cursor.execute(accounts_table)
+    cursor.execute(transactions_table)
+
+    create_triggers(cursor)
 
     database.commit()
     database.close()
+
+def create_triggers(cursor):
+    check_trigger = """
+        CREATE TRIGGER check_transaction 
+        BEFORE UPDATE ON accounts FOR EACH ROW
+        BEGIN
+            IF NEW.currency < 0 THEN
+                SIGNAL SQLSTATE '45000' 
+                SET MESSAGE_TEXT = "CURRENCY NO PUEDE TENER BALANCE NEGATIVO";
+            END IF;
+        END;
+    """
+
+    register_trigger = """
+        CREATE TRIGGER register_transaction 
+        AFTER UPDATE ON accounts FOR EACH ROW
+        BEGIN
+            DECLARE v_balance INT;
+
+            SET v_balance = NEW.currency - OLD.currency;
+
+            INSERT INTO transactions (username, old_currency, new_currency, date, time, balance)
+            VALUES (NEW.username, OLD.currency, NEW.currency, CURRENT_DATE, CURRENT_TIME, v_balance);
+        END;
+    """
+
+    cursor.execute(check_trigger)
+    cursor.execute(register_trigger)
